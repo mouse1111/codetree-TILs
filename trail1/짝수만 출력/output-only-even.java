0bead0be0d7e40c2258642c338936f4d0a8b3772
Park JuHee <117792716+mouse1111@users.noreply.github.com>
2026-06-01 12:37:44 +0900

    [Passed] 짝수만 출력 (127ms, 10MB)

---
+++ trail1/짝수만 출력/output-only-even.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        for(int i=A; i<=B; i++){
+            if(i%2==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
